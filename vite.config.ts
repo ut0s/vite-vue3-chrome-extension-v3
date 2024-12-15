c3346b7f94fbaa942b250a6efa47d6677ca82c3b
Yuto Tagashira <ut0s.dev@gmail.com>
2024-12-15 21:21:49 +0900

    feat: remove console output on minify

--- vite.config.ts
+++ vite.config.ts
@@ -121,6 +121,13 @@ export default defineConfig({
     }),
   ],
   build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
     rollupOptions: {
       input: {
         iframe: 'src/content-script/iframe/index.html',
