0c9588316dc264de2261ffd1d055bf37d901df4d
Yuto Tagashira <ut0s.dev@gmail.com>
2025-01-08 00:20:43 +0900

    feat: make side panels height 100%

--- src/ui/side-panel/app.vue
+++ src/ui/side-panel/app.vue
@@ -2,13 +2,15 @@
 
 <template>
   <UApp>
-    <AppHeader />
+    <div class="flex flex-col h-full w-full">
+      <AppHeader />
 
-    <div class="p-4 prose dark:prose-invert">
-      <RouterView />
+      <div class="p-4 prose dark:prose-invert">
+        <RouterView />
+      </div>
+      <div class="grow" />
+      <AppFooter />
     </div>
-
-    <AppFooter />
   </UApp>
 </template>
 
